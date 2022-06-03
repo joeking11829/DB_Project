420ec80e2f736eb42434c3eea140cde6d2b806b2
pete_pan <pete_pan@hongtech.com.tw>
2022-06-03 22:06:33 +0800

    php - more pages class_edit.php teacher_edit.php

--- html/index.php
+++ html/index.php
@@ -4,7 +4,9 @@
 <?php include 'mysql.php';
 //dump_table($sql, "Student");
 ?>
-<a href="/student_edit.php" target="學生維護頁面" title="title 學生維護頁面">學生維護頁面</a>
+<a href="student_edit.php"  title="title 學生維護頁面">學生維護頁面</a><br><br>
+<a href="class_edit.php"  title="title 課程維護頁面">課程維護頁面</a><br><br>
+<a href="teacher_edit.php" title="title 老師維護頁面">老師維護頁面</a><br>
 </center>
 </body>
 </html>
